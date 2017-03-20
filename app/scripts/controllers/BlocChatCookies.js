46784b67f1e04a23b3d7f18bfe209ebd7ed8e21a
Behic Akgun <behica@gmail.com>
2017-03-20 15:52:41 -0500

    Used prompt to set username

---
+++ app/scripts/controllers/BlocChatCookies.js
@@ -0,0 +1,18 @@
+(function() {
+  function BlocChatCookies($cookies) {
+    var currentUser = $cookies.get('blocChatCurrentUser');
+    if (!currentUser || currentUser === '') {
+      // Do something to allow users to set their username
+        currentUser = prompt("Please enter your user name:");
+        while(currentUser === "" || currentUser === null){
+            currentUser = prompt("You did not enter a valid user name. Please try again.");
+        }
+        console.log(currentUser);
+    }
+  }
+
+  angular
+    .module('blocChat')
+    .run(['$cookies', BlocChatCookies]);
+})();
+
--- dist/scripts/app.js
+++ dist/scripts/app.js
@@ -16,6 +16,6 @@
     }
     
     angular
-        .module('blocChat', ['ui.router', 'firebase'])
+        .module('blocChat', ['ui.router', 'firebase', 'ngCookies'])
         .config(config);
 })();
